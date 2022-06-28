27a4ad45af1f9bf5cb3abed49c06b15c4c928fd0
jeevanreddym <jeevan.rocks@gmail.com>
2022-06-28 11:57:46 -0400

    spring boot basics.

--- src/test/java/com/example/demo/MySpringbootBasicsApplicationTests.java
+++ src/test/java/com/example/demo/MySpringbootBasicsApplicationTests.java
@@ -19,6 +19,7 @@ class MySpringbootBasicsApplicationTests {
 	@Autowired
 	private MockMvc mvc;
 
+
 	@Test
 	public void getHello() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
